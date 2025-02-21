77aa584a6dc50285b6c715df64f6b3326d7e88ca
hassan khaled <hasan.khaled.fcis@gmail.com>
2025-02-21 13:24:43 +0200

    feat(add_basic_function): Add basic function pages for the service worker module
     adding two files as module and eanble the extension to carry out basic function

--- sw-tips.js
+++ sw-tips.js
@@ -1,1 +1,16 @@
 console.log("sw-tips.js");
+// Save default API suggestions
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === 'install') {
+    chrome.storage.local.set({
+      apiSuggestions: ['tabs', 'storage', 'scripting']
+    });
+  }
+});
+
+async function updateHistory(input) {
+  const { apiSuggestions } = await chrome.storage.local.get('apiSuggestions');
+  apiSuggestions.unshift(input);
+  apiSuggestions.splice(NUMBER_OF_PREVIOUS_SEARCHES);
+  return chrome.storage.local.set({ apiSuggestions });
+}
